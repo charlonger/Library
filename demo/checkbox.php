048d9fc13c053c7b15e838cea114be105f058271
charlonger <hdreaml@126.com>
2014-09-22 09:16:57 +0800

    后台类库

---
+++ demo/checkbox.php
@@ -0,0 +1,13 @@
+<?php
+$list = [
+    ['id'=>1, 'title'=>'中国国旅', 'checked'=>1],
+    ['id'=>2, 'title'=>'驰誉网络', 'checked'=>0],
+    ['id'=>3, 'title'=>'海南假日', 'checked'=>1],
+    ['id'=>4, 'title'=>'长安度假', 'checked'=>0],
+    ['id'=>5, 'title'=>'西部风景', 'checked'=>1],
+];
+?>
+
+
+
+
---
+++ demo/template/sele.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * author      : huanglong
+ * createTime  : 14-9-15 上午11:19
+ * description : 
+ */ 
